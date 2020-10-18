20e47ffc7a9f9e8e83ce79fb5937dd988b881393
jean pier <69778955+jeanPROangeles@users.noreply.github.com>
2020-10-18 17:13:14 -0500

    solo faltaría retornar en el return jsonify todos lo que queremos

--- app.py
+++ app.py
@@ -11,13 +11,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://student@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:jeanpierrenzo1234@localhost:5432/utec'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios_nuevos'
@@ -31,7 +28,9 @@ class Usuario(db.Model):
     def __repr__(self):
         return f'<Usuario: {self.id}, {self.dzname}, {self.dzEmail}, {self.dzOther_phone}, {self.dzOther_sex}, {self.dzMessage}>'
 
-@app.route('/todos/create', methods=['POST'])
+db.create_all()
+
+@app.route('/usuarios_nuevos/create', methods=['POST'])
 def create_usuario():
     print("usuario_creado")
     try:
@@ -40,17 +39,17 @@ def create_usuario():
         dzOther_phone = request.get_json()['dzOther_phone']
         dzOther_sex = request.get_json()['dzOther_sex']
         dzMessage = request.get_json()['dzMessage']
-        
+        print(dzname, dzEmail, dzOther_phone, dzOther_sex, dzMessage)
 
         usuario = Usuario(dzname = dzname, dzEmail = dzEmail, dzOther_phone = dzOther_phone, dzOther_sex= dzOther_sex, dzMessage = dzMessage)
         db.session.add(usuario)
         db.session.commit()
         return jsonify({
-            "dzname": usuario.dzname
-            "dzEmail" : usuario.dzEmail
-        })
+            "dzname": usuario['dzname'],
+            })
     except:
         db.session.rollback()
+        return "hubo un problema"
     finally:
         db.session.close()
 
